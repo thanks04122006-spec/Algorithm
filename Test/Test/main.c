d2239e3ed23decd5102028c5d6cf982e5db5717e
thanks04122006-spec <thanks04122006@gmail.com>
2026-04-03 09:56:07 +0900

    Add Project1

---
+++ Test/Test/main.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+//알고리즘 실습 첫번째수업
+//2026 이가영
+//2026.03.09
+
+int main(void)
+{
+	int age = 21;
+
+	printf("저는 ai소프트웨어학과 이가영입니다. \n");
+	printf("나는 %d살입니다." , age);
+
+	return 0;
+}
